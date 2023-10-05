63a4f4d79012a32d2dd943a07aabd8d33907a31b
obaidashfaq20 <obaidashfaq20@gmail.com>
2023-10-05 18:36:06 +0500

    Expose auth token

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,8 +9,9 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins "*"
 
-    resource "*",
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+    resource '*', headers: :any, methods: [:get, :post, :options, :patch, :delete], expose: ['Authorization']
+    # resource "*",
+    #   headers: :any,
+    #   methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
