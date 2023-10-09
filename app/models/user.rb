60896536afe42ec94f71282b019389f38a80916e
obaidashfaq20 <obaidashfaq20@gmail.com>
2023-10-09 12:54:53 +0500

    Add timeout

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  devise :timeoutable
   include Devise::JWT::RevocationStrategies::JTIMatcher
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
