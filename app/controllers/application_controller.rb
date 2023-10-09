a2ec76e9b5a434b3689cb932e9e0e3a1eaf86a67
obaidashfaq20 <obaidashfaq20@gmail.com>
2023-10-09 15:05:38 +0500

    Authenticate request with valid token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,22 @@
 class ApplicationController < ActionController::API
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!, unless: :devise_controller?
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[name email])
     devise_parameter_sanitizer.permit(:account_update, keys: %i[name email])
+  end
+
+  def authenticate_user!
+    if request.headers['Authorization'].present?
+      jwt_payload = JWT.decode(request.headers['Authorization'].split(' ').last, Rails.application.credentials.devise_jwt_secret_key!).first
+      current_user = User.find(jwt_payload['sub'])
+    else
+      render json: { error: 'Unauthorized' }, status: :unauthorized
+    end
+  rescue JWT::DecodeError
+    render json: { error: 'Invalid token' }, status: :unauthorized
   end
 end
