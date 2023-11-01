eb722e1b89ec7268cdc66cccb8d3ce977608b059
obaidashfaq20 <obaidashfaq20@gmail.com>
2023-11-01 12:50:06 +0500

    Lint properly

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -3,7 +3,9 @@
 class Users::SessionsController < Devise::SessionsController
   include RackSessionsFix
   respond_to :json
+
   private
+
   def respond_with(current_user, _opts = {})
     render json: {
       status: { 
@@ -12,6 +14,7 @@ class Users::SessionsController < Devise::SessionsController
       }
     }, status: :ok
   end
+
   def respond_to_on_destroy
     if request.headers['Authorization'].present?
       jwt_payload = JWT.decode(request.headers['Authorization'].split(' ').last, Rails.application.credentials.devise_jwt_secret_key!).first
