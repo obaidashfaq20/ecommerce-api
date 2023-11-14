2c5a17d2caab6e63229bfc9c7a61a84ce2249d65
obaidashfaq20 <obaidashfaq20@gmail.com>
2023-11-14 16:16:13 +0500

    Initail configuration for API documentation

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
   resources :carts
   resources :products
   resources :cart_items
