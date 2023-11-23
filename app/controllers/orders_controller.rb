05e5e57f0e1807a3f7e972ffcb36005fe1111e69
obaidashfaq20 <obaidashfaq20@gmail.com>
2023-11-23 15:27:38 +0500

    Create new order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,32 @@
 class OrdersController < ApplicationController
-  # GET /carts
+  before_action :set_product, only: %i[ show ]
+
+  # GET /orders
   def index
     @orders = current_user.orders
 
     render json: @orders
+  end
+
+  # GET /products/1
+  def show
+    render json: @order
+  end
+
+  # POST /orders
+  def create
+    @order = Order.new(user: current_user)
+
+    if @order.save
+      render json: @order, status: :created, location: @order
+    else
+      render json: @order.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_order
+    @order = Order.find(params[:id])
   end
 end
